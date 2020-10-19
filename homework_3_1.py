f941b5a59fd869c5701c081c972cb60ffa59f94d
hyeonki89 <44542466+hyeonki89@users.noreply.github.com>
2020-10-19 19:35:25 +0900

    Update homework_3_1.py

--- homework_3_1.py
+++ homework_3_1.py
@@ -20,12 +20,12 @@ my_data = []
 # 랭크 정보를 가져올 수 있었지만, 만약 랭크 정보가 없이 순서대로 있다면 랭크를 리스트에 추가하는 법
 my_rank = 0
 for tr in trs:
-    # 랭크 정보가 있는 경우
+    # 랭크 정보가 있는 경우 아래 코드 사용
     # rank = tr.select_one('#frm > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div > span.rank')
     title = tr.select_one('td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a').text
     album = tr.select_one('td:nth-child(6) > div > div > div > a').text
     artist = tr.select_one('td:nth-child(5) > div > div > div.ellipsis.rank02 > a').text
-    #랭크를 하나씩 추가해주는 방법
+    #랭크정보가 없다면 랭크를 하나씩 추가해주는 방법 
     my_rank = my_rank+1
 
     my_dict = {
